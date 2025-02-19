b432f97ab2a10279110f1b41416aa1b482812e02
Abraão Vieri <127057422+abvieri@users.noreply.github.com>
2025-02-19 15:14:38 -0300

    Delete CRUD_Python directory

--- CRUD_Python/app/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from app.models import User
-
-class UserForm(ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'phone' , 'email']
--- CRUD_Python/app/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User (models.Model):
-    username = models.CharField(max_length=20)
-    phone = models.IntegerField(max_length=11)
-    email = models.EmailField(max_length=254)
