edaa1d11b64383cc7a666156787390867d055284
abvieri <vieriabraao@gmail.com>
2025-02-19 15:57:34 -0300

    Update

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -76,10 +76,7 @@ WSGI_APPLICATION = 'CRUD.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('postgresql://crudpython_ejl8_user:YwD8vhsb6UdkNUHncZPG9HjgRxZjoHjm@dpg-cur29h5ds78s7384etr0-a/crudpython_ejl8'))
 }
 
 
